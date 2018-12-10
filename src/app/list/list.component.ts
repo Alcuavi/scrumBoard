af6d8b81223b542410d31ffeefe17323e003470c
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2018-12-10 13:47:12 +0100

    Añadida funcion new task

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'list',
@@ -7,4 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 export class ListComponent {
   @Input() listObj: object;
+  @Output() newTask = new EventEmitter();
+  newTaskText: string;
+  addNewTask() {
+    const text = this.newTaskText.trim();
+    if (text !== '') {
+      this.newTask.emit(this.newTaskText);
+      this.newTaskText = '';
+    }
+  }
 }
